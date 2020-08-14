a2b5c8c581b7e6fc4800fbb7fea650e123afd3ca
Кирилл Курко <kirillkurko2609@gmail.com>
2020-08-14 13:27:16 +0300

    Task finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
+    private static final Random randomGenerator = new Random();
+
+    public static int getRandomInt() {
+        return randomGenerator.nextInt(10);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    Matrix matrix = new Matrix(3);
+	    matrix.randomizeElements();
+	    matrix.printMatrix();
+	    double diagonalElementsRatio = matrix.findDiagonalElementsRatio();
+        System.out.println(diagonalElementsRatio);
     }
 }
--- src/com/company/Matrix.java
+++ src/com/company/Matrix.java
@@ -1,5 +1,58 @@
 package com.company;
 
 public class Matrix {
+
     private int size;
+
+    private int matrix[][];
+
+    public Matrix() {
+        size = 0;
+        matrix = null;
+    }
+
+    public Matrix(int size) {
+        this.size = size;
+        matrix = new int [size][size];
+    }
+
+    public void randomizeElements() {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                matrix[i][j] = Main.getRandomInt();
+            }
+        }
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public double findDiagonalElementsRatio() {
+        int mainDiagonalElementsSum = findMainDiagonalElementsSum();
+        int sideDiagonalElementsSum = findSideDiagonalElementsSum();
+        return (double) mainDiagonalElementsSum / sideDiagonalElementsSum;
+    }
+
+    private int findMainDiagonalElementsSum() {
+        int sum = 0;
+        for (int i = 0; i < size; ++i) {
+            sum += matrix[i][i];
+        }
+        return sum;
+    }
+
+    private int findSideDiagonalElementsSum() {
+        int sum = 0;
+        for (int i = 0; i < size; ++i) {
+            sum += matrix[i][size - i - 1];
+        }
+        return sum;
+    }
+
 }
